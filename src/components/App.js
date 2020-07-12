073dd6d30ccded632b5ee6a2578b6c54acbe2ca3
Chloe <chloe.e.muncy@gmail.com>
2020-07-12 14:47:12 -0400

    contests loaded

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,27 @@
 import React from 'react';
 import Header from './Header';
+import ContestPreview from './ContestPreview';
+import data from '../testData';
 
 // extend React.Component to have it be an element with a state instead of stateless
 class App extends React.Component {
     state = { 
-        pageHeader: 'Naming Contests'
+        pageHeader: 'Naming Contests',
+        contests: []
     };
     // lifecycle methods
+    componentDidMount() {
+        this.setState({
+            contests: data.contests
+        });
+        // axios.get('/api/contests')
+        //     .then(resp => {
+        //         this.setState({
+        //             contests: resp.data.contests
+        //         });
+        //     })
+        //     .catch(console.error);
+    }
     // componentDidMount() {
     //     integration code dependent upon DOM
     //     timers
@@ -21,7 +36,9 @@ class App extends React.Component {
             <div className="App">
                 <Header message={this.state.pageHeader} />
                 <div>
-                    ...
+                    {this.state.contests.map(contest =>
+                        <ContestPreview key={contest.id} {...contest} />
+                    )}
                 </div>
             </div>
         );  

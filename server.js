49785f3b64e30f50b14af405b1413bbe96e86f97
Chloe <chloe.e.muncy@gmail.com>
2020-07-12 14:57:19 -0400

    added sass

--- server.js
+++ server.js
@@ -1,8 +1,16 @@
 import config from './config';
 import apiRouter from './api';
+import sass from 'node-sass-middleware';
+import path from 'path';
 
 import express from 'express';
 const server = express();
+
+// sass middleware
+server.use(sass({
+    src: path.join(__dirname, 'sass'),
+    dest: path.join(__dirname, 'public')
+}));
 
 server.set('view engine', 'ejs');
 

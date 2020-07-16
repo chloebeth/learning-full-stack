b4faad312fcb0afe0c87396abc81baef5b73d352
Chloe <chloe.e.muncy@gmail.com>
2020-07-16 11:27:37 -0400

    change route on contest click

---
+++ src/components/Contest.js
@@ -0,0 +1,17 @@
+import React, { Component, PropTypes } from 'react';
+
+class Contest extends Component {
+    render() {
+        return (
+            <div className="Contest">
+                {this.props.id}
+            </div>
+        );
+    }
+}
+
+// Contest.propTypes = {
+//     id: React.PropTypes.number
+// };
+
+export default Contest;

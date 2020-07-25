96cf7f8cc797aa0e2219cba11eed645bb5e78219
Chloe <chloe.e.muncy@gmail.com>
2020-07-25 01:50:25 -0400

    init commit

--- Node/server.js
+++ Node/server.js
@@ -1,11 +1,17 @@
 const express = require('express');
-
+const path = require('path');
 const server = express();
 
 const port = 3000;
 
+server.use(express.static(path.join(__dirname, './static')));
+
 server.get('/', (req, res) => {
-    res.send('Hello express');
+    res.sendFile(path.join(__dirname, './static/index.html'));
+});
+
+server.get('/speakers', (req, res) => {
+    res.sendFile(path.join(__dirname, './static/speakers.html'));
 });
 
 server.listen(port, () => {

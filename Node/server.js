c827fc8237b4694536099a9aa4895c070848cf32
Chloe <chloe.e.muncy@gmail.com>
2020-07-26 19:10:37 -0400

    added ejs

--- Node/server.js
+++ Node/server.js
@@ -1,13 +1,17 @@
 const express = require('express');
 const path = require('path');
+
 const server = express();
 
 const port = 3000;
 
+server.set('view engine', 'ejs');
+server.set('views', path.join(__dirname, './views'));
+
 server.use(express.static(path.join(__dirname, './static')));
 
 server.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, './static/index.html'));
+  res.render('pages/index', { pageTitle: 'Welcome' });
 });
 
 server.get('/speakers', (req, res) => {
